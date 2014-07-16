8af7ce59cdb2242577521994b993e57f6846627d
Max Rogers <maxrogers90@gmail.com>
2014-07-16 12:58:26 -0400

    added sinatra

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "rake"
+
+group :development do 
+	gem 'shotgun'
+	gem "sqlite3"
+end
+
+group :production do 
+	gem "pg"
+end
---
+++ app.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model'        #Model class
+
+get '/' do
+  erb :index
+end
+
+get '/tut0' do
+	erb :tut0
+end
+
+after do
+  # Close the connection after the request is done so that we don't
+  # deplete the ActiveRecord connection pool.
+  ActiveRecord::Base.connection.close
+end
