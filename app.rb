3c9926b8686e26a20e656d90c7b090f929b91749
Max Rogers <maxrogers90@gmail.com>
2014-07-19 21:02:54 -0400

    removed old files

--- app.rb
+++ app.rb
@@ -11,6 +11,11 @@ get '/tut0' do
 	erb :tut0
 end
 
+get '/tut0b' do
+	erb :tut0b
+end
+
+
 after do
   # Close the connection after the request is done so that we don't
   # deplete the ActiveRecord connection pool.
